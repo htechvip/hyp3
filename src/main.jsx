7a74dffbeefe5c0980ce4d14909c69ed08d9629d
cho-nan tsai <sunnybook8@gmail.com>
2026-05-03 08:01:30 +0800

    added sentry

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import * as Sentry from "@sentry/react"
 import './index.css'
 import App from './App.jsx'
+
+Sentry.init({
+  dsn: "https://6c06fd10bf3118af7b257230d7c78dd2@o4511322979631104.ingest.us.sentry.io/4511322990772224",
+  sendDefaultPii: true,
+})
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
